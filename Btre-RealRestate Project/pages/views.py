3b574ebbf3b0d0eb98ffac390c8a8a4e2b9233d7
younusraza909 <younusraza909@gmail.com>
2020-04-30 11:39:40 -0700

    Managed Realtors and Seller of Month

--- Btre-RealRestate Project/pages/views.py
+++ Btre-RealRestate Project/pages/views.py
@@ -1,11 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from listings.models import Listing
+from realtors.models import Realtor
 # Create your views here.
 
 
 def index(request):
-    return render(request, "pages/index.html")
+    # [:3] to miniize date to get only 3
+
+    listings = Listing.objects.order_by("-list_date").filter(is_published=True)[:3]
+
+    context = {
+        'listings' : listings
+    }
+
+    return render(request, "pages/index.html", context)
 
 
 def about(request):
-    return render(request, "pages/about.html")
+    #Get all realtors
+    realtors=Realtor.objects.order_by("-hire_date")
+
+    #get MVP
+    mvp_realtors=Realtor.objects.all().filter(is_mvp=True)
+
+    context={
+        "realtors":realtors,
+        "mvp_realtors":mvp_realtors
+    }
+
+    return render(request, "pages/about.html",context)
