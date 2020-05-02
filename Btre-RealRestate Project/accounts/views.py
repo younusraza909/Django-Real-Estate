78f1c53133f1f2c1a6cfd0306aeee536a81947c9
younusraza909 <younusraza909@gmail.com>
2020-05-02 11:32:58 -0700

    User LoggedIn

--- Btre-RealRestate Project/accounts/views.py
+++ Btre-RealRestate Project/accounts/views.py
@@ -42,8 +42,17 @@ def register(request):
 
 def login(request):
     if request.method == "POST":
-        #Login User
-        pass
+        username=request.POST['username']
+        password=request.POST['password']
+
+        user=auth.authenticate(username=username,password=password)
+        if user is not None:
+            auth.login(request,user)
+            messages.success(request,"You are not logged in ..!")
+            return redirect("dashboard")
+        else:
+            messages.error(request,"Invalid Credentials..!")
+            return redirect("login")   
     else:    
         return render(request,"accounts/login.html")
 
