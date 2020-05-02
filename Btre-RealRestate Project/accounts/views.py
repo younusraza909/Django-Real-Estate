f9e2b3c9c2a7194358b3898e31ecf2a6a56cdad7
younusraza909 <younusraza909@gmail.com>
2020-05-02 11:17:57 -0700

    User Registeration

--- Btre-RealRestate Project/accounts/views.py
+++ Btre-RealRestate Project/accounts/views.py
@@ -1,11 +1,41 @@
 from django.shortcuts import render,redirect
-from django.contrib import messages
+from django.contrib import messages,auth
+from django.contrib.auth.models import User
 
 
 def register(request):
     if request.method == "POST":
-        messages.error(request,"Testing error message")
-        return redirect("register")
+        # Get Form Value
+        first_name =request.POST['first_name']
+        last_name =request.POST['last_name']
+        username =request.POST['username']
+        email =request.POST['email']
+        password =request.POST['password']
+        password2 =request.POST['password2']
+        
+        #Check If Password mathes
+        if password==password2:
+            if User.objects.filter(username=username).exists():
+                messages.error(request,"Username already taken..!")
+                return redirect("register")
+            else:
+                if User.objects.filter(email=email).exists():
+                    messages.error(request,"Email already taken ..!")
+                    return redirect("register")
+                else:
+                    user=User.objects.create_user(username=username,password=password,email=email,last_name=last_name,first_name=first_name)
+                    #LogIn User directly 
+                    # auth.login(request,user)
+                    # messages.success(request,"You are succesfully loggedIn ..!")
+                    # return redirect("index")    
+
+                    user.save()
+                    messages.success(request,"You are succesfully registerd ..!")
+                    return redirect("login")
+
+        else:
+            messages.error(request,"Both passwords should be matched..!")
+            return redirect('register')
     else:
         return render(request,"accounts/register.html")
 
